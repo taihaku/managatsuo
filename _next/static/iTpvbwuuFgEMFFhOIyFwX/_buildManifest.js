39753d748d5282404c2e90d6b5ed7f5ee6f9cca9
taihaku <taihaku@users.noreply.github.com>
2022-02-25 19:31:05 +0000

    deploy: efb51788bd4e2dcb507087eb0d33051746b90994

---
+++ _next/static/iTpvbwuuFgEMFFhOIyFwX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-844f32e3b3b81f36.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
